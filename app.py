dc65cdfd90739c3cee7840c5a74405c43fd3dd6e
choiywln <70733071+choiywln@users.noreply.github.com>
2020-12-18 20:23:50 +0900

    .

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+def main():
+    return render_template('main.html')
+
+
+@app.route('/purchase')
+def purchase():
+    return render_template('purchase.html')
+
+
+@app.route('/check')
+def check():
+    return render_template('check.html')
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def base():
+#     if request.method == "POST":
+#         return render_template('purchase.html')
+
+
+# @app.route('/purchase', methods=['GET', 'POST'])
+# def purchase():
+#     return render_template('purchase.html')
 
 
 if __name__ == '__main__':
